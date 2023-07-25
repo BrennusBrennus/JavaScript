8b40c5432c4516255f0536a019cdec26dec43173
Breno Henrique <brennus.haimrich@gmail.com>
2023-07-25 14:41:07 -0300

    condicionais if e else

---
+++ ex06/cond.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(`a velocidade do seu carro é de ${vel}km por hora`)
+if (vel > 60) { // condição simples
+    console.log('você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('diriga sempre usando cinto de segurança')
