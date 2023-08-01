14af6ae9ac8df65293ef092caa7f40a758ce1322
Breno Henrique <brennus.haimrich@gmail.com>
2023-08-01 16:53:07 -0300

    .

--- ex07/while.js
+++ ex07/while.js
@@ -1,23 +1,6 @@
 var c = 1
 
 do {
-    if (c % 2 == 1) {
-        console.log(`passo ${c}`)
-        c++
-    }
-} while (c <= 13)
-
-
-
-
-
-
-
-
-// var cont = 1
-// while (cont <= 5) {
-//     console.log('oi laurinha')
-//     cont++
-// }
-
-// console.log('e aí?')
+    console.log(`passo ${c}`)
+    c++
+} while(c <=3)
