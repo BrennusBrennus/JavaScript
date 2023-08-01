f6a8a4e9d75548ec387a1dcc2738a241d2f7a929
Breno Henrique <brennus.haimrich@gmail.com>
2023-08-01 07:33:38 -0300

    .

---
+++ ex07/node_5195c6cd3a586.tmp.js
@@ -0,0 +1,11 @@
+var cont = 1
+while (cont < 5) {
+    console.log('oi laurinha')
+}
+
+
+
+
+
+
+// console.log('e aí?')
---
+++ ex07/while.js
@@ -0,0 +1,12 @@
+var cont = 1
+while (cont < 5) {
+    console.log('oi laurinha')
+    cont ++
+}
+
+
+
+
+
+
+// console.log('e aí?')
